91117631283c201ed30666fd5b1f5faebfc465cb
irtaza010 <irtaza010@gmail.com>
2025-04-11 02:22:42 +0500

    fixe

--- src/store/services/booking.ts
+++ src/store/services/booking.ts
@@ -113,8 +113,7 @@ export const bookingApi = api.injectEndpoints({
     }),
     paymentCheck: build.query({
       query: ({reference, booking_id}) => ({
-        url: `/payment/return?reference=4a04ef37-9dfb-424e-95c0-f738a495a254&booking_id=40`,
-        // url: `/payment/return?reference=${reference}&booking_id=${booking_id}`,
+        url: `/payment/return?reference=${reference}&booking_id=${booking_id}`,
         method: "GET"
       }),
     }),
